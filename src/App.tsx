19d397e9fb25e2f066959e206107f76bc8c384cb
maarijwastaken <105754222+maarijwastaken@users.noreply.github.com>
2023-05-24 00:40:39 -0600

    Tried fixing prettier end-of-line error

--- src/App.tsx
+++ src/App.tsx
@@ -77,7 +77,7 @@ function App() {
           </header>
           <Game settingsData={settingsData} />
           <footer className="flex justify-center text-sm mt-8 mb-1">
-              <Leadle />
+            <Leadle />
           </footer>
         </div>
       </div>

73c3875ae26e489f8d6f39076d7d4ceecf2b797e
maarijwastaken <105754222+maarijwastaken@users.noreply.github.com>
2023-06-04 20:17:53 -0600

    Theoretically all TypeScript errors should be fixed now

--- src/hooks/getLeaderImage.ts
+++ src/hooks/getLeaderImage.ts
@@ -7,12 +7,18 @@ export function getLeaderImage(img_link: string) : string {
       const html = data.parse.text["*"];
       const parser = new DOMParser();
       const doc = parser.parseFromString(html, "text/html");
-      let infoboxImageURL, fullSizeInfoboxImageURL;
+      let infoboxImageURL, fullSizeInfoboxImageURL, queriedLink: HTMLLinkElement;
       if (!(doc.querySelector(".redirectMsg"))) {
         if (doc.querySelector(".infobox-full-data a"))
-          infoboxImageURL = doc.querySelector(".infobox-full-data a").href;
+        {
+          queriedLink = doc.querySelector(".infobox-full-data a")!;
+          infoboxImageURL = queriedLink.href;
+        }
         else if (doc.querySelector(".infobox-image a"))
-          infoboxImageURL = doc.querySelector(".infobox-image a").href;
+        {
+          queriedLink = doc.querySelector(".infobox-image a")!;
+          infoboxImageURL = queriedLink.href;
+        }
         else // It's probably San Marino
           infoboxImageURL = "https://en.wikipedia.org/wiki/Ham_sandwich#/media/File:Ham_sandwich1.jpg" // ham sandwich is the fallback
         fullSizeInfoboxImageURL = "https://commons.wikimedia.org/wiki/Special:FilePath/" + infoboxImageURL.split("File:").pop()
@@ -22,15 +28,21 @@ export function getLeaderImage(img_link: string) : string {
       }
       else {
         console.log("Used alternative! Damn redirects...")
-        const altLink = "https://corsproxy.io/?https://en.wikipedia.org/w/api.php?action=parse&page=" + doc.querySelector("a").href.split("wiki/").pop() + "&format=json"
+        const altLink = "https://corsproxy.io/?https://en.wikipedia.org/w/api.php?action=parse&page=" + doc.querySelector("a")!.href.split("wiki/").pop() + "&format=json"
         fetch(altLink).then(response => response.json()).then(data => {
           const html = data.parse.text["*"];
           const parser = new DOMParser();
           const doc = parser.parseFromString(html, "text/html");
           if (doc.querySelector(".infobox-full-data a"))
-            infoboxImageURL = doc.querySelector(".infobox-full-data a").href;
+          {
+            queriedLink = doc.querySelector(".infobox-full-data a")!;
+            infoboxImageURL = queriedLink.href;
+          }
           else if (doc.querySelector(".infobox-image a"))
-            infoboxImageURL = doc.querySelector(".infobox-image a").href;
+          {
+            queriedLink = doc.querySelector(".infobox-image a")!;
+            infoboxImageURL = queriedLink.href;
+          }
           else // It's probably San Marino
             infoboxImageURL = "https://en.wikipedia.org/wiki/Ham_sandwich#/media/File:Ham_sandwich1.jpg" // ham sandwich is the fallback
           fullSizeInfoboxImageURL = "https://commons.wikimedia.org/wiki/Special:FilePath/" + infoboxImageURL.split("File:").pop()
@@ -38,7 +50,6 @@ export function getLeaderImage(img_link: string) : string {
           return fullSizeInfoboxImageURL;
         })
       }
-      return "https://en.wikipedia.org/wiki/Ham_sandwich#/media/File:Ham_sandwich1.jpg" // If all goes wrong, ham sandwich
     })
-  
+    return "https://en.wikipedia.org/wiki/Ham_sandwich#/media/File:Ham_sandwich1.jpg" // If all goes wrong, ham sandwich
 }
